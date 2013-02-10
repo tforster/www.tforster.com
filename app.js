a24d1227fa888997483f0f67c1d32e89df9a3cee
Troy Forster <troy.forster@gmail.com>
2013-02-09 20:26:06 -0500

    basic CRU working

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var http = require("http");
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
 var app = express();
+var server = http.createServer(app);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function () {
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.set('view options', { layout: false });
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+   app.use(app.router);
+   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function () {
+   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.configure('production', function () {
+   app.use(express.errorHandler());
 });
+
+var articleProvider = new ArticleProvider("localhost", 27017);
+
+
+
+app.get('/', function (req, res) {
+   articleProvider.findAll(function (error, docs) {
+      res.render('index.jade', {
+         title: 'Blog',
+         articles: docs
+      });
+   })
+});
+
+app.get('/blog/new', function (req, res) {
+   res.render('blog_new.jade', {
+
+      title: 'New Post'
+
+   });
+});
+
+app.post('/blog/new', function (req, res) {
+   articleProvider.save({
+      title: req.param('title'),
+      body: req.param('body')
+   }, function (error, docs) {
+      res.redirect('/')
+   });
+});
+
+app.get('/blog/:id', function (req, res) {
+   articleProvider.findById(req.params.id, function (error, article) {
+      res.render('blog_show.jade',
+      {
+         title: article.title,
+         article: article
+      });
+   });
+});
+
+
+app.post('/blog/addComment', function (req, res) {
+   articleProvider.addCommentToArticle(req.param('_id'), {
+      person: req.param('person'),
+      comment: req.param('comment'),
+      created_at: new Date()
+   }, function (error, docs) {
+      res.redirect('/blog/' + req.param('_id'))
+   });
+});
+
+
+server.listen(3000);
+console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
