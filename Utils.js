c3f095b5eac5ef0868b2ba46a9e5250649c6591f
Troy Forster <troy.forster@gmail.com>
2013-03-04 00:02:41 -0500

    trying again

---
+++ Utils.js
@@ -0,0 +1,21 @@
+﻿Utils = function () { };
+
+Utils.prototype.exists = function (value, otherwise) {
+   if (typeof (otherwise) === "undefined") {
+      return "Parameter otherwise was not defined.";
+   }
+   else {
+      return ((typeof (value) !== "undefined") && (value)) ? value : otherwise;
+   }
+}
+
+Utils.prototype.Sluggify = function (title) {
+   return title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+}
+
+Utils.prototype.test = function (callback) {
+   console.log("TEST");
+};
+
+
+exports.Utils = Utils;
